6bc12c6102208b032895cef60c8941346e8b6f60
Vaibhav071799 <163979570+Vaibhav071799@users.noreply.github.com>
2025-09-23 19:40:27 +0530

    Create Test.java
    Adding java code

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+  public static void add(){
+    int a = 5, b=10;
+    int result =  a+b;
+
+    System.out.println("Addition of a + b is " + result );
+  }
+  public static void main (String[] args){
+    add();
+  }
+}
